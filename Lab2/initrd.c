ef1db81a423cdfcfb25675616c3db75fe8938de0
Ryan <ht177970@gmail.com>
2025-09-30 11:49:53 +0800

    Added cpio walk and ls

---
+++ Lab2/initrd.c
@@ -0,0 +1,42 @@
+#include "initrd.h"
+#include "utils.h"
+
+#ifdef __QEMU__
+#define CPIO_ADDR 0x8000000
+#else
+#define CPIO_ADDR 0x21000000
+#endif
+
+void* cpio_base = (void*)(CPIO_ADDR);
+
+int pad4(int x){
+    return (4-(x&3))&3;
+}
+
+char* next(const char* now){
+    if(strncmp(now+sizeof(struct cpio_newc_header), "TRAILER!!!", 10) == 0)
+        return 0;
+    struct cpio_newc_header* header = (struct cpio_newc_header*)now;
+    int namesz = HtoI(header->c_namesize, 8);
+    int datasz = HtoI(header->c_filesize, 8);
+    int headsz = sizeof(struct cpio_newc_header)+namesz;
+    headsz += pad4(headsz);
+    datasz += pad4(datasz);
+    return (char*)(now+headsz+datasz);
+}
+
+void initrd_list(){
+    const char* now = cpio_base;
+    const char* nxt = 0;
+    while((nxt = next(now)) != 0){
+        struct cpio_newc_header* header = (struct cpio_newc_header*)now;
+        int namesz = HtoI(header->c_namesize, 8);
+        char* tmp = now+sizeof(struct cpio_newc_header);
+        for(int i = 0;i < namesz;i++){
+            uart_putc(*tmp);
+            tmp++;
+        }
+        uart_putc('\n');
+        swap(now, nxt);
+    }
+}
