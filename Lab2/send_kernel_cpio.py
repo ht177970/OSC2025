283ba36dbe800bcfd7413bb8689ea5d4ff1d0056
Ryan <ht177970@gmail.com>
2025-09-30 13:54:42 +0800

    Added a script to send both kernel and cpio

---
+++ Lab2/send_kernel_cpio.py
@@ -0,0 +1,41 @@
+from serial import Serial
+import time
+
+def getBinary(x):
+    ret = ''
+    c01 = "01"
+    while x > 0:
+        ret += c01[x % 2]
+        x >>= 1
+    while len(ret) < 32:
+        ret += c01[0]
+    return ret[::-1].encode()
+
+st = time.time()
+with Serial("/dev/ttyUSB0", 115200) as ser:
+    with open('build/kernel.bin', 'rb') as f:
+        raw = f.read()
+        length = len(raw)
+        #send kernel size in binary
+        print("Start to send kernel size to UART in binary.")
+        ser.write(getBinary(length))
+        ser.flush()
+        
+        print("Start to send kernel to UART in byte.")
+        for i in range(length):
+            ser.write(raw[i:i+1])
+            ser.flush()
+            print(f"{i+1}/{length} bytes sent.")
+
+    with open('initramfs.cpio', 'rb') as f:
+        raw = f.read()
+        length = len(raw)
+        ser.write(getBinary(length))
+        ser.flush()
+        for i in range(length):
+            ser.write(raw[i:i+1])
+            ser.flush()
+            print(f"{i+1}/{length} bytes sent.")
+    print('OK')
+print(f'total time: {time.time()-st} s')
+
